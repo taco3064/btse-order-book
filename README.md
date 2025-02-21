93daca4ca40c87ab637e2ebdfced284987452eb3
taco3064 <tabacotaco@gmail.com>
2025-02-21 14:24:13 +0800

    style(*): add tailwind css, and init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # btse-order-book
+
 A real-time order book built with React, displaying buy and sell quotes dynamically.
+
+## Getting Started
+
+- **Node.js Version**: `22.14.0`
+- Install dependencies after checkout: `npm ci`
+- Start the development server: `npm run dev`
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
 });
