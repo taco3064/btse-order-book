50108ec71c2054cdd16205928f7d8c6c1969628d
taco3064 <tabacotaco@gmail.com>
2025-02-22 21:20:43 +0800

    chore(order-book): add reducer commands

--- src/containers/OrderBook/hooks.ts
+++ src/containers/OrderBook/hooks.ts
@@ -82,26 +82,32 @@ const useOrderReducer = (() => {
   function updateEntries(
     type: OrderEntry['type'],
     curr: OrderEntry[],
-    arr: OrderEntryArray[],
+    arr: OrderEntryArray[], //* WebSocket 回傳的 Entry 更新紀錄
   ) {
     return arr.reduce(
       (acc, entryArray) => {
         const { price, size } = getOrderEntry(type, entryArray);
 
         if (size && price > acc[0]?.price) {
+          //* 新增的 Entry 有最高價格 (必須有 size)
           acc.unshift({ type, price, size });
         } else if (size && price < acc[acc.length - 1]?.price) {
+          //* 新增的 Entry 有最低價格 (必須有 size)
           acc.push({ type, price, size });
         } else {
+          //* 取得最新 Entry 應插入的位置
           const index = acc.findIndex(
             ({ price: p }, i) => p === price || (p > price && acc[i + 1]?.price < price),
           );
 
           if (size && acc[index]?.price !== price) {
+            //* 新增的 Entry (必須有 size)
             acc.splice(index + 1, 0, { type, price, size });
           } else if (!size && acc[index]?.price === price) {
+            //* 已存在的 Entry 且 size 為 0，則刪除
             acc.splice(index, 1);
           } else if (size) {
+            //* 更新已存在的 Entry (必須有 size)
             acc[index] = { type, price, size };
           }
         }
@@ -118,6 +124,7 @@ const useOrderReducer = (() => {
         if (seqNum > state.seq) {
           switch (type) {
             case 'snapshot':
+              //* 初始取得的 Entry 資料。已由大而小排序，所以前端只需要將 price / size 轉換成數字即可
               return {
                 seq: seqNum,
                 asks: asks.map((ask) => getOrderEntry('ask', ask)),
@@ -125,6 +132,7 @@ const useOrderReducer = (() => {
               };
 
             case 'delta':
+              //* 持續更新的 Entry 資料
               return {
                 seq: seqNum,
                 asks: updateEntries('ask', state.asks, asks),
