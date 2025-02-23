4f5fddd7d4e45950d8eb8433d641bb88d6d35cd6
taco3064 <tabacotaco@gmail.com>
2025-02-23 15:47:27 +0800

    build(deploy.yml): add github action

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
 
+import { name } from './package.json';
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV !== 'production' ? '/' : `/${name}`,
   plugins: [react(), tsconfigPaths(), tailwindcss()],
 });
